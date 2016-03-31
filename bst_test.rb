d24e4ad1344baa1c17e6973653a08095d90af74f
Anna Weisbrodt <aweisbro@gmail.com>
2016-03-30 20:14:14 -0600

    tests added

--- bst_test.rb
+++ bst_test.rb
@@ -1,6 +1,6 @@
 require "minitest/autorun"
 require "minitest/pride"
-require "./bst"
+require "./bst_new"
 
 class BinarySearchTreeTest < Minitest::Test
 
@@ -9,31 +9,93 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal BinarySearchTree, tree.class
   end
 
-  def test_valid_bst_exists
+  # def test_bst_starts_empty
+  #   tree = BinarySearchTree.new
+  #   assert_equal nil, tree.root
+  # end
+  #
+  # def test_left_starts_at_zero
+  #   tree = BinarySearchTree.new
+  #   assert_equal nil, tree.left
+  # end
+
+  def test_tree_has_root
     tree = BinarySearchTree.new
-    #all lower values are to the left, higher to the right
+    tree.insert(13, "Test1")
+    assert tree.root
+    assert_equal 13, tree.root.key
   end
 
-  test_bst_nodes_consist_of_key_value_pairs
+  def test_tree_has_correct_root
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(8, "Test2")
+    tree.insert(25, "Test3")
+    tree.insert(88, "Test4")
+    assert tree.root
+    assert_equal 13, tree.root.key
+  end
 
-  test_inserted_items_are_present
+  def test_tree_has_right_branch
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(25, "Test2")
+    assert_equal 25, tree.root.right.key
+  end
 
-  test_can_insert_new_items
+  def test_tree_has_left_branch
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(8, "Test2")
+    assert_equal 8, tree.root.left.key
+  end
 
-  test_includes
-    #finds expected values
-    #does not find unexpected values
+  def test_tree_has_left_branch
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(8, "Test2")
+    tree.insert(3, "Test3")
+    assert_equal 3, tree.root.left.left.key
+  end
 
-  test_can_find_depth_of
+  def test_include_works
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(8, "Test2")
+    tree.insert(3, "Test3")
+    assert_equal tree.include?(8), true
+  end
 
-  test_can_find_max
+  # def test_insert
+  #   tree = BinarySearchTree.new
+  #   tree.insert(13, "Test1")
+  #
+  #   assert_equal true, tree.include?(13, "Test1")
+  # end
 
-  test_can_find_min
+  # def test_search
+  #   tree = BinarySearchTree.new
 
-  test_can_sort
+  # test_bst_nodes_consist_of_key_value_pairs
 
-  test_can_load
-
-  test_health
+  # test_inserted_items_are_present
+  #
+  # test_can_insert_new_items
+  #
+  # test_includes
+  #   #finds expected values
+  #   #does not find unexpected values
+  #
+  # test_can_find_depth_of
+  #
+  # test_can_find_max
+  #
+  # test_can_find_min
+  #
+  # test_can_sort
+  #
+  # test_can_load
+  #
+  # test_health
 
 end
