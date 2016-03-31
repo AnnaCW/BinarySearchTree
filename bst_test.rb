aab215bcb796843dc823c6972f1cf2d8d360c5e4
Anna Weisbrodt <aweisbro@gmail.com>
2016-03-31 01:27:43 -0600

    sort test added

--- bst_test.rb
+++ bst_test.rb
@@ -56,6 +56,21 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal tree.include?(8), true
   end
 
+  def test_insert_depth_0
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    assert_equal tree.insert(13, "Test1"), 0
+  end
+
+  def test_insert_depth_1
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(71, "Test3")
+    assert_equal tree.insert(20, "Test2"), 1
+    assert_equal tree.insert(71, "Test3"), 1
+  end
+
   def test_depth_of_root
     tree = BinarySearchTree.new
     tree.insert(13, "Test1")
@@ -71,7 +86,7 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal tree.depth_of(71), 1
   end
 
-  def test_depth_of_level_2
+  def test_depth_of_2
     tree = BinarySearchTree.new
     tree.insert(61, "Test1")
     tree.insert(20, "Test2")
@@ -82,36 +97,46 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal tree.depth_of(80), 2
   end
 
-  # def test_insert
-  #   tree = BinarySearchTree.new
-  #   tree.insert(13, "Test1")
-  #
-  #   assert_equal true, tree.include?(13, "Test1")
-  # end
+  def test_depth_of_3
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(71, "Test3")
+    tree.insert(5, "Test4")
+    tree.insert(80, "Test5")
+    tree.insert(2, "Test4")
+    tree.insert(85, "Test5")
+    assert_equal tree.depth_of(2), 3
+    assert_equal tree.depth_of(85), 3
+  end
 
-  # def test_search
-  #   tree = BinarySearchTree.new
+  def test_can_find_max
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(71, "Test3")
+    assert_equal tree.max, {71 => "Test3"}
+  end
 
-  # test_bst_nodes_consist_of_key_value_pairs
+  def test_can_find_min
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(8, "Test3")
+    tree.insert(71, "Test4")
+    assert_equal tree.min, {8 => "Test3"}
+  end
 
-  # test_inserted_items_are_present
-  #
-  # test_can_insert_new_items
-  #
-  # test_includes
-  #   #finds expected values
-  #   #does not find unexpected values
-  #
-  # test_can_find_depth_of
-  #
-  # test_can_find_max
-  #
-  # test_can_find_min
-  #
-  # test_can_sort
-  #
-  # test_can_load
-  #
-  # test_health
+  def test_can_sort
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(8, "Test3")
+    tree.insert(71, "Test4")
+
+    expected = [{8 => "Test3"}, {20 => "Test2"}, {61 => "Test1"}, {71 => "Test4"}]
+
+    assert_equal expected, tree.sort
+  end
 
 end
