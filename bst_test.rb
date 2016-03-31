8aa82e3054b058f4c52e39201dfb34e7554cf5ee
Anna Weisbrodt <aweisbro@gmail.com>
2016-03-30 22:01:36 -0600

    tests added

--- bst_test.rb
+++ bst_test.rb
@@ -9,16 +9,6 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal BinarySearchTree, tree.class
   end
 
-  # def test_bst_starts_empty
-  #   tree = BinarySearchTree.new
-  #   assert_equal nil, tree.root
-  # end
-  #
-  # def test_left_starts_at_zero
-  #   tree = BinarySearchTree.new
-  #   assert_equal nil, tree.left
-  # end
-
   def test_tree_has_root
     tree = BinarySearchTree.new
     tree.insert(13, "Test1")
@@ -58,12 +48,38 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal 3, tree.root.left.left.key
   end
 
-  def test_include_works
+  def test_include_method_works
     tree = BinarySearchTree.new
     tree.insert(13, "Test1")
     tree.insert(8, "Test2")
     tree.insert(3, "Test3")
     assert_equal tree.include?(8), true
+  end
+
+  def test_depth_of_root
+    tree = BinarySearchTree.new
+    tree.insert(13, "Test1")
+    assert_equal tree.depth_of(13), 0
+  end
+
+  def test_depth_of_1
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(71, "Test3")
+    assert_equal tree.depth_of(20), 1
+    assert_equal tree.depth_of(71), 1
+  end
+
+  def test_depth_of_level_2
+    tree = BinarySearchTree.new
+    tree.insert(61, "Test1")
+    tree.insert(20, "Test2")
+    tree.insert(71, "Test3")
+    tree.insert(5, "Test4")
+    tree.insert(80, "Test5")
+    assert_equal tree.depth_of(5), 2
+    assert_equal tree.depth_of(80), 2
   end
 
   # def test_insert
